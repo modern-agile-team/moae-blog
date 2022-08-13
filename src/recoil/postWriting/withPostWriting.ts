0c01ab3b479be3fb649fa874d2971d444b7dd61d
soonki-98 <alstnsrl98@gmail.com>
2022-08-13 09:19:48 +0900

    fix: MarkdownPostHeader type 오류 수정

--- src/recoil/postWriting/withPostWriting.ts
+++ src/recoil/postWriting/withPostWriting.ts
@@ -1,7 +1,8 @@
 import { selector } from "recoil";
 import postWriting from "./atom";
+import { IPostWritingType } from "./type";
 
-const withPostWriting = selector({
+const withPostWriting = selector<IPostWritingType>({
   key: "withPostWriting",
   get: ({ get }) => get(postWriting),
   set: ({ set }, newValue) => set(postWriting, newValue),
