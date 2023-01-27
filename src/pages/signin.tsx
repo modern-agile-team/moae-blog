c9569f5b25863b964a4c3cd7a60c2269ed15719d
soonki-98 <alstnsrl98@gmail.com>
2023-01-27 23:49:07 +0900

    feat: 구글 로그인 성공시 백엔드로 api 요청

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,9 +1,35 @@
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 import * as Layout from "@component/Layout";
 import styled from "styled-components";
+import { useEffect } from "react";
+import { useMutation } from "react-query";
+import { login } from "@core/apis";
+import CONST from "@constant/index";
 
 const SignIn = () => {
+  const { data, status } = useSession();
+
+  console.log("::::", data, status);
+
+  const { mutate } = useMutation(login, {
+    onSuccess(data, variables, context) {
+      console.log(":::::", data);
+    },
+    onError(error, variables, context) {
+      console.error(error);
+    },
+  });
+
+  useEffect(() => {
+    if (status === CONST.SESSION_STATUS.AUTHENTICATED) {
+      if (!data.user) return;
+      const { email, name, image } = data.user;
+
+      mutate({ email: email!, name: name!, baseUrl: image! });
+    }
+  }, []);
+
   return (
     <Layout.Login>
       <LoginButton onClick={() => signIn("google")}>구글로 로그인하기</LoginButton>
