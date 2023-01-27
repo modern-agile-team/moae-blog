5bbe45631ee8050b24f9f5de21305c89fa96aaca
soonki-98 <alstnsrl98@gmail.com>
2023-01-27 22:55:09 +0900

    remove: 어드민 관련 파일 제거

--- src/component/Admin/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as AdminWrapper } from "./Wrapper";
-export { default as AdminInput } from "./Input";
-export { default as AdminButton } from "./Submit";
-export { default as LeftSide } from "./LeftSide";
-export { default as RightSide } from "./RightSide";
-export { default as EmailItem } from "./EmailItem";
-export { default as EmailList } from "./EmailList";
