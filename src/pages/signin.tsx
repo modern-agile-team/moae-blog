c3ebdf28f7ceb54a4e414cfc882da4ac6a476e55
soonki-98 <alstnsrl98@gmail.com>
2023-01-28 00:11:41 +0900

    feat: 로그인 성공시 홈 화면으로 라우팅 처리

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,31 +1,29 @@
 import { signIn, useSession } from "next-auth/react";
 import { AxiosError, AxiosResponse } from "axios";
-
-import * as Layout from "@component/Layout";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import { useEffect } from "react";
 import { useMutation } from "react-query";
+
+import * as Layout from "@component/Layout";
 import { login } from "@core/apis";
 import CONST from "@constant/index";
 
 const SignIn = () => {
   const { data, status } = useSession();
-
-  console.log("::::", data, status);
+  const router = useRouter();
 
   const { mutate } = useMutation(login, {
     onSuccess(response: AxiosResponse<{ accessToken: string; refreshToken: string }>, variables, context) {
       const { data } = response;
-      console.log(":::::::success", response);
       sessionStorage.setItem("accessToken", data.accessToken);
       sessionStorage.setItem("refreshToken", data.refreshToken);
+      router.push("/");
     },
     onError(error: AxiosError, variables, context) {
       console.error(error.response?.data, variables, context);
     },
   });
-
-  console.log("::::::token", sessionStorage.getItem("accessToken"), sessionStorage.getItem("refreshToken"));
 
   useEffect(() => {
     if (status === CONST.SESSION_STATUS.AUTHENTICATED) {
