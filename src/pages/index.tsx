c8a5cfbbca92d46220bc803e4e6bc74917c5e5ad
soonki-98 <alstnsrl98@gmail.com>
2023-01-31 14:45:40 +0900

    feat: 게시글 불러오기 SSR 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,41 @@
 import { uuid } from "uuidv4";
+import { useQuery, QueryClient, dehydrate } from "react-query";
+//@ts-ignore
+import safeJsonStringify from "safe-json-stringify";
 
 import { Card } from "@component/Common";
 import HotPosts from "@component/HotPosts/HotPosts";
-import { cardProps } from "@constant/test";
 import * as L from "@component/Layout";
+import * as APIS from "@core/apis";
+import { Loader } from "@component/Common/Loader";
 
 const randomSize = () => {
   return Math.floor(Math.random() * 1000);
 };
 
-interface UserInfoType {
-  profileImage: string;
-  name: string;
-}
+const Home = () => {
+  const { isLoading, error, data } = useQuery("getAllBoards", APIS.BOARDS.getAll, {
+    refetchOnWindowFocus: false,
+  });
 
-interface CardType {
-  id: string;
-  title: string;
-  description: string;
-  date: string;
-  userInfo: UserInfoType;
-}
-interface HomeProps {
-  posts: CardType[];
-}
-
-const Home = ({ posts }: HomeProps) => {
   return (
     <div>
       <HotPosts />
       <L.Card>
-        {posts.map((el) => (
-          <Card key={uuid()} {...el} titleImage={`https://picsum.photos/${randomSize()}/${randomSize()}`} />
-        ))}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          data!.data.map((post) => (
+            <Card
+              key={uuid()}
+              title={post.title}
+              id={post.id.toString()}
+              date={post.createdAt}
+              description={post.context}
+              titleImage={`https://picsum.photos/${randomSize()}/${randomSize()}`}
+            />
+          ))
+        )}
       </L.Card>
     </div>
   );
@@ -41,11 +44,7 @@ const Home = ({ posts }: HomeProps) => {
 export default Home;
 
 export async function getServerSideProps() {
-  const posts = [];
-
-  for (let i = 0; i < 30; i++) {
-    posts.push(cardProps);
-  }
-
-  return { props: { posts } };
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery("getAllBoards", APIS.BOARDS.getAll);
+  return { props: { dehydratedState: JSON.parse(safeJsonStringify(dehydrate(queryClient))) } };
 }
