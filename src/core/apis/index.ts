5620d1c8a3f2f706b840087849b38bedb86aebcb
soonki-98 <alstnsrl98@gmail.com>
2023-01-31 14:45:58 +0900

    feat: 게시글 불러오기 API 생성

--- src/core/apis/index.ts
+++ src/core/apis/index.ts
@@ -1,7 +1,23 @@
 import * as T from "@type/index";
+import { AxiosResponse } from "axios";
 import instance from "./instance";
 
 export const login = async (data: T.API.Request.Login) => {
   const result = await instance.post("/auth/sign-in", data);
   return result;
+};
+interface PostType {
+  context: string;
+  createdAt: string;
+  id: number;
+  title: string;
+  updatedAt: string;
+  userId: number;
+}
+
+export const BOARDS = {
+  async getAll() {
+    const result: AxiosResponse<PostType[]> = await instance.get("/board/all");
+    return result;
+  },
 };
