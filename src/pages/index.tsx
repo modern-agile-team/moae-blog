ad4211b027c958a40defe367acb4938cf3c18356
soonki-98 <alstnsrl98@gmail.com>
2023-01-31 16:36:36 +0900

    rollback: hydrate 방식으로 롤백

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,27 +8,14 @@ import HotPosts from "@component/HotPosts/HotPosts";
 import * as L from "@component/Layout";
 import * as APIS from "@core/apis";
 import { Loader } from "@component/Common/Loader";
-import { AxiosResponse } from "axios";
 
 const randomSize = () => {
   return Math.floor(Math.random() * 1000);
 };
-interface PostType {
-  context: string;
-  createdAt: string;
-  id: number;
-  title: string;
-  updatedAt: string;
-  userId: number;
-}
-interface HomeProps {
-  postData: AxiosResponse<PostType[]>;
-}
 
-const Home = (props: HomeProps) => {
+const Home = () => {
   const { isLoading, error, data } = useQuery("getAllBoards", APIS.BOARDS.getAll, {
     refetchOnWindowFocus: false,
-    initialData: props.postData,
   });
 
   return (
@@ -38,7 +25,7 @@ const Home = (props: HomeProps) => {
         {isLoading ? (
           <Loader />
         ) : (
-          data?.data.map((post) => (
+          data!.data.map((post) => (
             <Card
               key={uuid()}
               title={post.title}
@@ -59,5 +46,5 @@ export default Home;
 export async function getServerSideProps() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery("getAllBoards", APIS.BOARDS.getAll);
-  return { props: { postData: JSON.parse(safeJsonStringify(dehydrate(queryClient).queries[0].state.data)) } };
+  return { props: { dehydratedState: JSON.parse(safeJsonStringify(dehydrate(queryClient))) } };
 }
