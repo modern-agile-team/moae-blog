cba70cf165a03645432d1841e2321d46ca223108
Min <alstnsrl98@gmail.com>
2023-02-09 00:33:01 +0900

    feat: 마크다운 코드 폰트사이즈 변경

--- src/component/Posts/MarkdownViewer.tsx
+++ src/component/Posts/MarkdownViewer.tsx
@@ -1,10 +1,12 @@
+import { useEffect } from "react";
 import { Viewer } from "@toast-ui/react-editor";
+import Prism from "prismjs";
+
+import "prismjs/themes/prism.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css";
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
-import Prism from "prismjs";
-import "prismjs/themes/prism.css";
 
 interface Props {
   initialValue: string;
@@ -12,6 +14,13 @@ interface Props {
 }
 
 const MarkdownViewer = ({ initialValue, theme = "light" }: Props) => {
+  useEffect(() => {
+    const codes = Array.from(document.getElementsByTagName("code"));
+    for (const code of codes) {
+      code.style.fontSize = "20px";
+    }
+  }, []);
+
   return (
     <Viewer
       theme={theme}
