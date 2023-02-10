425de400e627d5f77178e760fcd6ed1e1fc0a5b5
soonki-98 <alstnsrl98@gmail.com>
2023-02-10 22:37:24 +0900

    feat: API 주소 변경

--- src/core/apis/index.ts
+++ src/core/apis/index.ts
@@ -2,34 +2,70 @@ import * as T from "@type/index";
 import { AxiosResponse } from "axios";
 import instance from "./instance";
 
-export const login = async (data: T.API.Request.Login) => {
-  const result = await instance.post("/auth/sign-in", data);
-  return result;
-};
-
 export const USER = {
-  defaultPath: "/auth",
+  path: "/auth",
   async login(data: T.API.Request.Login) {
-    const result = await instance.post(`${USER.defaultPath}/sign-in`, data);
+    const result = await instance.post(`${USER.path}/sign-in`, data);
     return result;
   },
   async checkAuth() {
-    const result = await instance.get(`${USER.defaultPath}/existence`);
+    const result = await instance.get(`${USER.path}/existence`);
+    return result;
+  },
+  async refresh() {
+    const result: AxiosResponse<{ accessToken: string; refreshToken: string }> = await instance.post(
+      `${USER.path}/refresh`
+    );
     return result;
   },
 };
 
 export const BOARDS = {
+  path: "/boards",
   async getAll() {
-    const result: AxiosResponse<T.POST.PostType[]> = await instance.get("/board/all");
+    const result: AxiosResponse<T.POST.PostType[]> = await instance.get(`${BOARDS.path}`);
     return result;
   },
   async create(params: T.POST.CreatePostType) {
-    const result: AxiosResponse<T.POST.PostType[]> = await instance.post("/board", params);
+    const result: AxiosResponse<T.POST.PostType[]> = await instance.post(`${BOARDS.path}`, params);
     return result;
   },
   async getPost(postId: string) {
-    const result: AxiosResponse<T.POST.PostType> = await instance.get(`/board/${postId}`);
+    const result: AxiosResponse<T.POST.PostType> = await instance.get(`${BOARDS.path}/${postId}`);
     return result;
   },
+  async modifyPost(postId: string, params: T.POST.CreatePostType) {
+    const result: AxiosResponse<T.POST.PostType> = await instance.patch(`${BOARDS.path}/${postId}`, params);
+    return result;
+  },
+  async deletePost(postId: string) {
+    const result: AxiosResponse<T.POST.PostType> = await instance.delete(`${BOARDS.path}/${postId}`);
+    return result;
+  },
+};
+
+export const COMMENT = {
+  path: "/board",
+  async getComments(postId: string) {
+    const result: AxiosResponse<any> = await instance.get(`${BOARDS.path}/${postId}/comments`);
+    return result;
+  },
+  async createComment(postId: string) {
+    const result: AxiosResponse<any> = await instance.post(`${BOARDS.path}/${postId}/comments`);
+    return result;
+  },
+  async modifyComment(postId: string, commentId: string) {
+    const result: AxiosResponse<any> = await instance.put(`${BOARDS.path}/${postId}/comments/${commentId}`);
+    return result;
+  },
+  async deleteComment(postId: string, commentId: string) {
+    const result: AxiosResponse<any> = await instance.delete(`${BOARDS.path}/${postId}/comments/${commentId}`);
+    return result;
+  },
+};
+
+export default {
+  USER,
+  BOARDS,
+  COMMENT,
 };
