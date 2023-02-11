adc8ffa31d2f994e5172aece0f0a468a60c186fe
soonki-98 <alstnsrl98@gmail.com>
2023-02-12 02:18:14 +0900

    feat: 전역 상수 호출 방식 변경

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,9 +1,3 @@
-import DEVICE from "./device";
-import SESSION_STATUS from "./session";
-
-const CONST = {
-  DEVICE,
-  SESSION_STATUS,
-} as const;
-
-export default CONST;
+export { default as DEVICE } from "./device";
+export { default as SESSION_STATUS } from "./session";
+export { default as API_KEYS } from "./apisKeys";
