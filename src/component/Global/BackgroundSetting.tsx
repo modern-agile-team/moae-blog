438514f26db45ad55bb8db98c6406adbb3c70c2b
soonki-98 <alstnsrl98@gmail.com>
2023-02-12 12:51:12 +0900

    feat: axios instance header setting 위치 변결 기존 BackgroundSetting.tsx 변경 후 axios instance 내부

--- src/component/Global/BackgroundSetting.tsx
+++ src/component/Global/BackgroundSetting.tsx
@@ -1,16 +1,7 @@
-import { useEffect } from "react";
-
 import { useDevice } from "@hooks/index";
-import { setAxiosAuthHeader, getToken } from "@utils/index";
 
 const BackgroundSetting = ({ children }: { children: React.ReactNode }) => {
-  const token = getToken();
-
   useDevice();
-
-  useEffect(() => {
-    setAxiosAuthHeader(token?.accessToken || "");
-  }, [token]);
 
   return <>{children}</>;
 };
--- src/core/apis/instance.ts
+++ src/core/apis/instance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    Authorization: "",
+    Authorization: `Bearer ${(typeof window !== "undefined" && localStorage.getItem("accessToken")) || ""}`,
   },
 });
 
