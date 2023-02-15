562b3ee2bacf2b11307e2fffeb05bbd448a2cec8
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-15 19:30:33 +0900

    refactor: Carousel 하단부 CardLayout 반응형 코드 추가 #4

--- src/component/Layout/Card.tsx
+++ src/component/Layout/Card.tsx
@@ -1,19 +1,22 @@
 import styled from "styled-components";
 
 const CardLayout = styled.div`
-  padding: 0 30px;
+  padding: 0 76px;
   display: grid;
   place-items: center;
   grid-template-columns: repeat(5, 1fr);
   gap: 15px;
   width: 100%;
-  @media (max-width: 1024px) {
+  @media (max-width: 1440px) {
     grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
   }
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media (max-width: 480px) {
+  @media (max-width: 568px) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
