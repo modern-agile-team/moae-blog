ca692cbe2017f97392e7cf1ed05acc4e6db25833
Sung Je Kim <rlatjdwp0211@naver.com>
2023-02-15 19:27:33 +0900

    modify: constant/device.ts에 LAP_TOP key 추가 #4

--- src/core/constant/device.ts
+++ src/core/constant/device.ts
@@ -1,7 +1,14 @@
 const DEVICE = {
-  DESK_TOP: "desktop",
-  MOBILE: "mobile",
-  TABLET: "tablet",
+  DESK_TOP: "desktop", // 1920
+  LAP_TOP: "laptop", // 1440
+  TABLET: "tablet", // 1024
+  MOBILE: "mobile", // 768
 } as const;
 
 export default DEVICE;
+
+// 1920: 5
+// 1440: 4
+// 1024: 3
+// 768: 2
+// 568: 1
