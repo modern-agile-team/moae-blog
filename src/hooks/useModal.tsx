c24569b01dc8490c676537c36affd21c249aa6ee
soonki-98 <alstnsrl98@gmail.com>
2023-02-15 20:37:25 +0900

    feat: useModal customhook 생성 #2

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,30 @@
+import { useSetRecoilState } from "recoil";
+
+import modalList from "@recoil/modalList";
+import ModalType from "@core/types/modal";
+
+const useModal = () => {
+  const setModalList = useSetRecoilState(modalList);
+
+  const showModal = (modalName: ModalType) => {
+    setModalList((prev) => {
+      if (prev.length > 1) {
+        return [...prev, modalName];
+      } else {
+        return [modalName];
+      }
+    });
+  };
+
+  const resetModal = () => {
+    setModalList([]);
+  };
+
+  const hideModal = (modalName: ModalType) => {
+    setModalList((prev) => [...prev].filter((modal) => modal !== modalName));
+  };
+
+  return { showModal, resetModal, hideModal };
+};
+
+export default useModal;
