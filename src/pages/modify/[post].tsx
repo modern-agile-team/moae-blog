f4216b46f08329c75ee054386652f9739350d3e0
soonki-98 <alstnsrl98@gmail.com>
2023-03-01 22:04:35 +0900

    feat: 게시글 수정 페이지 생성 #1

---
+++ src/pages/modify/[post].tsx
@@ -0,0 +1,66 @@
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+
+import { PostEditor, MarkdownPostHeader } from "@component/MarkdownRender";
+import SubmitContainer from "@component/MarkdownRender/SubmitContainer";
+import { Loader } from "@component/Common/Loader";
+import deviceAtom from "@recoil/deviceAtom";
+import theme from "@styles/theme";
+import { useCheckAuth } from "@hooks/index";
+
+const PostModifyPage = () => {
+  const device = useRecoilValue(deviceAtom);
+  const { isLoading } = useCheckAuth(() => {
+    alert("로그인 이후에 이용할 수 있습니다.");
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <MarkdownPostHeader />
+          <PostWrapper>
+            <PostEditor />
+          </PostWrapper>
+          {device === "mobile" && <SubmitContainer />}
+        </>
+      )}
+    </div>
+  );
+};
+
+const PostWrapper = styled.div`
+  padding: 0 6rem;
+  min-height: 90vh;
+  pre {
+    padding: 0;
+    margin: 20px 0 28px;
+  }
+  .MoaeBlogEditor {
+    min-height: 90vh;
+  }
+  .ProseMirror,
+  .toastui-editor-contents,
+  .toastui-editor-md-preview {
+    font-size: 16px;
+    &::-webkit-scrollbar {
+      width: 3px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: ${theme.COLORS.MAIN};
+    }
+  }
+  @media (max-width: 568px) {
+    padding: 0;
+    .ProseMirror,
+    .toastui-editor-contents,
+    .toastui-editor-md-preview {
+      font-size: 14px;
+      padding: 0;
+    }
+  }
+`;
+
+export default PostModifyPage;
